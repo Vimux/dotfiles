417c86e776de4f19d102e576e6e1897eb0706b72
vimux <vimux@protonmail.com>
2017-08-22 09:51:10 +0300

    Update macos.sh: add 'boot macOS in verbose mode' option

--- macos.sh
+++ macos.sh
@@ -8,6 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Boot macOS in verbose mode
+sudo nvram boot-args="-v"
+
 ###############################################################################
 # General                                                                     #
 ###############################################################################
